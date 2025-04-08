d1a20626bda9db3780d6ec0874364cd915f30598
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-04-08 14:55:31 +0000

    This is my calculation of the area of a trapozoids

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by:Shem
+// Created on:April 2025
 // This file contains the JS functions for index.html
+"use strict";
+      // Function to calculate area and perimeter
+      function calculate() {
+        // Get the input values
+        let height = parseFloat(document.getElementById("height-of-triangle").value);
+        let sideA = parseFloat(document.getElementById("side-a").value);
+        let sideB = parseFloat(document.getElementById("side-b").value);
+
+        // Calculate Area
+        let area = (sideA + sideB)/2 * height;
+
+        // Display the results
+        document.getElementById("area").innerHTML = "Area " + area.toFixed(1) + "cm^2";
+      }
